5ec6cfd032f581ecda47bc392a2e5a9627b533fa
TatianaKucheba <tanya.kucheba@gmailcom>
2023-07-02 18:39:39 +0600

    feat: solve 05-task

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -1,145 +1,127 @@
-/* ************************************************************************************************
- *                                                                                                *
- * Please read the following tutorial before implementing tasks:                                   *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
- *                                                                                                *
- ************************************************************************************************ */
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
 
+  function getArea() {
+    return this.width * this.height;
+  }
 
-/**
- * Returns the rectangle object with width and height parameters and getArea() method
- *
- * @param {number} width
- * @param {number} height
- * @return {Object}
- *
- * @example
- *    const r = new Rectangle(10,20);
- *    console.log(r.width);       // => 10
- *    console.log(r.height);      // => 20
- *    console.log(r.getArea());   // => 200
- */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+  this.getArea = getArea;
 }
 
-
-/**
- * Returns the JSON representation of specified object
- *
- * @param {object} obj
- * @return {string}
- *
- * @example
- *    [1,2,3]   =>  '[1,2,3]'
- *    { width: 10, height : 20 } => '{"height":10,"width":20}'
- */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
-
-/**
- * Returns the object of specified type from JSON representation
- *
- * @param {Object} proto
- * @param {string} json
- * @return {object}
- *
- * @example
- *    const r = fromJSON(Circle.prototype, '{"radius":10}');
- *
- */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  return Object.assign(Object.create(proto), obj);
 }
 
+class CssSelectorBuilder {
+  constructor() {
+    this.elements = [];
+    this.combinators = [];
+    this.isElementUsed = false;
+    this.isIdUsed = false;
+    this.isClassUsed = false;
+    this.isAttrUsed = false;
+    this.isPseudoClassUsed = false;
+    this.isPseudoElementUsed = false;
+  }
 
-/**
- * Css selectors builder
- *
- * Each complex selector can consists of type, id, class, attribute, pseudo-class
- * and pseudo-element selectors:
- *
- *    element#id.class[attr]:pseudoClass::pseudoElement
- *              \----/\----/\----------/
- *              Can be several occurrences
- *
- * All types of selectors can be combined using the combination ' ','+','~','>' .
- *
- * The task is to design a single class, independent classes or classes hierarchy
- * and implement the functionality to build the css selectors using the provided cssSelectorBuilder.
- * Each selector should have the stringify() method to output the string representation
- * according to css specification.
- *
- * Provided cssSelectorBuilder should be used as facade only to create your own classes,
- * for example the first method of cssSelectorBuilder can be like this:
- *   element: function(value) {
- *       return new MySuperBaseElementSelector(...)...
- *   },
- *
- * The design of class(es) is totally up to you, but try to make it as simple,
- * clear and readable as possible.
- *
- * @example
- *
- *  const builder = cssSelectorBuilder;
- *
- *  builder.id('main').class('container').class('editable').stringify()
- *    => '#main.container.editable'
- *
- *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
- *    => 'a[href$=".png"]:focus'
- *
- *  builder.combine(
- *      builder.element('div').id('main').class('container').class('draggable'),
- *      '+',
- *      builder.combine(
- *          builder.element('table').id('data'),
- *          '~',
- *           builder.combine(
- *               builder.element('tr').pseudoClass('nth-of-type(even)'),
- *               ' ',
- *               builder.element('td').pseudoClass('nth-of-type(even)')
- *           )
- *      )
- *  ).stringify()
- *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
- *
- *  For more examples see unit tests.
- */
+  validateSingleUsage(selectorType) {
+    if (selectorType) {
+      if (selectorType === 'element' && this.isElementUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+      if (selectorType === 'id' && this.isIdUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+      if (selectorType === 'class' && this.isClassUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+      if (selectorType === 'attr' && this.isAttrUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+      if (selectorType === 'pseudoClass' && this.isPseudoClassUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+      if (selectorType === 'pseudoElement' && this.isPseudoElementUsed) {
+        throw new Error(
+          'Element, id, and pseudo-element should not occur more than one time inside the selector',
+        );
+      }
+    }
+  }
 
-const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
-  },
+  element(value) {
+    this.validateSingleUsage('element');
+    this.elements.push(value);
+    this.isElementUsed = true;
+    return this;
+  }
 
-  id(/* value */) {
-    throw new Error('Not implemented');
-  },
+  id(value) {
+    this.validateSingleUsage('id');
+    this.elements.push(`#${value}`);
+    this.isIdUsed = true;
+    return this;
+  }
 
-  class(/* value */) {
-    throw new Error('Not implemented');
-  },
+  class(value) {
+    this.validateSingleUsage('class');
+    this.elements.push(`.${value}`);
+    this.isClassUsed = true;
+    return this;
+  }
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
-  },
+  attr(value) {
+    this.validateSingleUsage('attr');
+    this.elements.push(`[${value}]`);
+    this.isAttrUsed = true;
+    return this;
+  }
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
-  },
+  pseudoClass(value) {
+    this.validateSingleUsage('pseudoClass');
+    this.elements.push(`:${value}`);
+    this.isPseudoClassUsed = true;
+    return this;
+  }
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
-  },
+  pseudoElement(value) {
+    this.validateSingleUsage('pseudoElement');
+    this.elements.push(`::${value}`);
+    this.isPseudoElementUsed = true;
+    return this;
+  }
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
-  },
+  combine(selector1, combinator, selector2) {
+    this.combinators.push(`${selector1}${combinator}${selector2}`);
+    return this;
+  }
+
+  stringify() {
+    return `${this.elements.join('')}${this.combinators.join('')}`;
+  }
+}
+
+const cssSelectorBuilder = new CssSelectorBuilder();
+
+module.exports = {
+  cssSelectorBuilder,
 };
-
 
 module.exports = {
   Rectangle,
